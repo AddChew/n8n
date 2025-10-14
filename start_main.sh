1128301411de5ef0c8b9b9dfb6181f2c64145637
AddChew <addison_chew@outlook.com>
2025-10-14 18:21:51 +0800

    add workers

---
+++ start_main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -o allexport 
+source .main.env 
+set +o allexport
+
+/home/addison/projects/n8n/node_modules/.bin/n8n
--- start_worker.sh
+++ start_worker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source .env
+set -o allexport 
+source .worker.env 
+set +o allexport
 
-/home/addison/projects/n8n/node_modules/.bin/n8n &&
 /home/addison/projects/n8n/node_modules/.bin/n8n worker
+
 # /home/addison/projects/n8n/node_modules/.bin/n8n webhook
 # uv run pgadmin4
